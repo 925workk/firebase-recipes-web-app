a558b95a56233748ef652e8d2ebd7dc6ff05c788
Alex Pham <925workk@gmail.com>
2022-08-17 13:26:23 -0600

    added secrets to yaml.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import "./App.css";
+import { useState } from "react";
+import FirebaseAuthService from "./FirebaseAuthService";
 
-import firebase from "./FirebaseConfig";
+import "./App.css";
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  FirebaseAuthService.subscribeToAuthChanges(setUser);
+
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
+        <LoginForm existingUser={user} />
       </div>
     </div>
   );
