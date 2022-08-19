cd08dd036efd505a98fad2c27926deae14d9a455
Alex Pham <925workk@gmail.com>
2022-08-19 16:12:47 -0600

    added sorting.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,280 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FirebaseAuthService from "./FirebaseAuthService";
+import LoginForm from "./components/LoginForm";
+import AddEditRecipeForm from "./components/AddEditRecipeForm";
 
 import "./App.css";
-import LoginForm from "./components/LoginForm";
+import FirebaseFirestoreService from "./FirebaseFirestoreService";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [currentRecipe, setCurrentRecipe] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+  const [categoryFilter, setCategoryFilter] = useState("");
+  const [orderBy, setOrderBy] = useState("publishDateDesc");
+
+  useEffect(() => {
+    fetchRecipes()
+      .then((fetchedRecipes) => {
+        setRecipes(fetchedRecipes);
+      })
+      .catch((error) => {
+        console.error(error.message);
+        throw error;
+      });
+  }, [user, categoryFilter, orderBy]);
 
   FirebaseAuthService.subscribeToAuthChanges(setUser);
+
+  async function handleAddRecipe(newRecipe) {
+    try {
+      const response = await FirebaseFirestoreService.createDocument(
+        "recipes",
+        newRecipe
+      );
+
+      // TODO: fetch new recipes from firestore
+      handleFetchRecipes();
+
+      alert(`Successfully created a recipe with an ID = ${response.id}`);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  async function fetchRecipes() {
+    const queries = [];
+
+    if (categoryFilter) {
+      queries.push({
+        field: "category",
+        condition: "==",
+        value: categoryFilter,
+      });
+    }
+
+    if (!user) {
+      queries.push({
+        field: "isPublished",
+        condition: "==",
+        value: true,
+      });
+    }
+
+    const orderByField = "publishDate";
+    let orderByDirection;
+    if (orderBy) {
+      switch (orderBy) {
+        case "publishDateAsc":
+          orderByDirection = "asc";
+          break;
+        case "publishDateDesc":
+          orderByDirection = "desc";
+          break;
+        default:
+          break;
+      }
+    }
+
+    let fetchedRecipes = [];
+
+    try {
+      const response = await FirebaseFirestoreService.readDocuments({
+        collection: "recipes",
+        queries: queries,
+        orderByField: orderByField,
+        orderByDirection: orderByDirection,
+      });
+
+      const newRecipes = response.docs.map((recipeDoc) => {
+        const id = recipeDoc.id;
+        const data = recipeDoc.data();
+        data.publishDate = new Date(data.publishDate.seconds * 1000);
+
+        return { ...data, id };
+      });
+
+      fetchedRecipes = [...newRecipes];
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+
+    return fetchedRecipes;
+  }
+
+  async function handleFetchRecipes() {
+    try {
+      const fetchedRecipes = await fetchRecipes();
+      setRecipes(fetchedRecipes);
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+  }
+
+  async function handleUpdateRecipe(newRecipe, recipeId) {
+    try {
+      await FirebaseFirestoreService.updateDocument(
+        "recipes",
+        recipeId,
+        newRecipe
+      );
+      handleFetchRecipes();
+
+      alert(`successfully updated a recipe with an ID = ${recipeId}`);
+      setCurrentRecipe(null);
+    } catch (error) {
+      alert(error.message);
+      throw error;
+    }
+  }
+
+  async function handleDeleteRecipe(recipeId) {
+    const deleteConfirmation = window.confirm(
+      "Are you sure you want to delete this recipe? OK for Yes, Cancel for No"
+    );
+
+    if (deleteConfirmation) {
+      try {
+        await FirebaseFirestoreService.deleteDocument("recipes", recipeId);
+        handleFetchRecipes();
+        setCurrentRecipe(null);
+        window.scrollTo(0, 0);
+        alert(`successfully deleted a recipe with ID of ${recipeId}`);
+      } catch (error) {
+        alert(error.message);
+        throw error;
+      }
+    }
+  }
+
+  function handleEditRecipeClick(recipeId) {
+    const selectedRecipe = recipes.find((recipe) => {
+      return recipe.id === recipeId;
+    });
+    console.log(selectedRecipe);
+
+    if (selectedRecipe) {
+      try {
+        setCurrentRecipe(selectedRecipe);
+        window.scrollTo(0, document.body.scrollHeight);
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+
+  function handleEditRecipeCancel() {
+    setCurrentRecipe(null);
+  }
+
+  function lookupCategoryLabel(categoryKey) {
+    const categories = {
+      breadsSandwichesAndPizza: "Breads, Sandwiches, and Pizza",
+      eggsAndBreakfast: "Eggs & Breakfast",
+      dessertsAndBakedGoods: "Desserts & Baked Goods",
+      fishAndSeafood: "Fish & Seafood",
+      vegetables: "Vegetables",
+    };
+
+    const label = categories[categoryKey];
+    return label;
+  }
+
+  function formatDate(date) {
+    const day = date.getUTCDate();
+    const month = date.getUTCMonth() + 1;
+    const year = date.getUTCFullYear();
+
+    const dateString = `${month}-${day}-${year}`;
+    return dateString;
+  }
 
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
-        <LoginForm existingUser={user} />
+        <LoginForm existingUser={user}></LoginForm>
+      </div>
+      <div className="main">
+        <div className="row filters">
+          <label className="recipe-label input-label">
+            Category:
+            <select
+              value={categoryFilter}
+              onChange={(e) => setCategoryFilter(e.target.value)}
+              className="select"
+              required
+            >
+              <option value=""></option>
+              <option value="breadsSandwichesAndPizza">
+                Breads, Sandwiches, and Pizza
+              </option>
+              <option value="eggsAndBreakfast">Eggs & Breakfast</option>
+              <option value="dessertsAndBakedGoods">
+                Desserts & Baked Goods
+              </option>
+              <option value="fishAndSeafood">Fish & Seafood</option>
+              <option value="vegetables">Vegetables</option>
+            </select>
+          </label>
+          <label className="input-label">
+            <select
+              value={orderBy}
+              onChange={(e) => setOrderBy(e.target.value)}
+            >
+              <option value="publishDateDesc">
+                Publish Date (newest - older)
+              </option>
+              <option value="publishDateAsc">
+                Publish Date (oldest - newest)
+              </option>
+            </select>
+          </label>
+        </div>
+        <div className="center">
+          <div className="recipe-list-box">
+            {recipes && recipes.length > 0 ? (
+              <div className="recipe-list">
+                {recipes.map((recipe) => {
+                  return (
+                    <div className="recipe-card" key={recipe.key}>
+                      {recipe.isPublished === false ? (
+                        <div className="unpublished">UNPUBLISHED</div>
+                      ) : null}
+                      <div className="recipe-name">{recipe.name}</div>
+                      <div className="recipe-field">
+                        Category: {lookupCategoryLabel(recipe.category)}
+                      </div>
+                      <div className="recipe-field">
+                        Publish Date: {formatDate(recipe.publishDate)}
+                      </div>
+                      {user ? (
+                        <button
+                          type="button"
+                          onClick={() => handleEditRecipeClick(recipe.id)}
+                          className="primary-button edit-button"
+                        >
+                          EDIT
+                        </button>
+                      ) : null}
+                    </div>
+                  );
+                })}
+              </div>
+            ) : null}
+          </div>
+        </div>
+        {user ? (
+          <AddEditRecipeForm
+            existingRecipe={currentRecipe}
+            handleAddRecipe={handleAddRecipe}
+            handleUpdateRecipe={handleUpdateRecipe}
+            handleEditRecipeCancel={handleEditRecipeCancel}
+            handleDeleteRecipe={handleDeleteRecipe}
+          ></AddEditRecipeForm>
+        ) : null}
       </div>
     </div>
   );
